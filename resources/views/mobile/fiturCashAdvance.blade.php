2f593a26e483a0f6d9d6a46a1ca8a84688a0260e
superevilstockholm <balilombok11@gmail.com>
2025-09-24 15:15:53 +0700

    Created user guide page for cash advance

--- resources/views/mobile/fiturCashAdvance.blade.php
+++ resources/views/mobile/fiturCashAdvance.blade.php
@@ -12,91 +12,90 @@
                             perusahaan kepada
                             karyawan
                             untuk keperluan tertentu, seperti perjalanan dinas atau kebutuhan mendesak lainnya.
-
                             <br><br>
                             <b>Cash Advance</b> berfungsi sebagai dana talangan sementara untuk kebutuhan mendesak karyawan
                             atau operasional, yang nantinya akan dipertanggungjawabkan atau dipotong dari gaji karyawan
                             sesuai
                             dengan kebijakan perusahaan.
-
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
         <!-- /Page Header -->
-
         {{-- Page Content --}}
-        <div class="row g-3">
+        <div class="row g-5">
             <div class="col-12">
-                <h4 class="fw-semibold">1. Buka aplikasi <b>Pehadir</b> dan pastikan berada di menu <b>Home</b>. Selanjutnya
+                <h4 class="fw-normal mb-4">1. Buka aplikasi <b>Pehadir</b> dan pastikan berada di menu <b>Home</b>.
+                    Selanjutnya
                     klik menu <b>More</b>.</h4>
-                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi1.png') }}" alt="Fitur KPI">
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/1.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">2. Selanjutnya, cari dan pilih menu <b>Cash Advance</b> pada tampilan <b>More
+                <h4 class="fw-normal mb-4">2. Selanjutnya, cari dan pilih menu <b>Cash Advance</b> pada tampilan <b>More
                         Menu.</b></h4>
-                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi2.png') }}" alt="Fitur KPI">
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/2.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">3. Klik tombol (+) di pojok kanan bawah untuk membuat permintaan baru.</h4>
-                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi3.png') }}" alt="Fitur KPI">
+                <h4 class="fw-normal mb-4">3. Klik tombol (+) di pojok kanan bawah untuk membuat permintaan baru.</h4>
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/3.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">4. Cash Advance Type</h4>
+                <h4 class="fw-normal mb-4">4. Cash Advance Type</h4>
                 <ul>
                     <li><i class="bi bi-dot"></i> Pilih jenis kasbon yang ingin diajukan (contoh: KASBON).</li>
                 </ul>
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">5. Amount</h4>
+                <h4 class="fw-normal mb-4">5. Amount</h4>
                 <ul>
                     <li><i class="bi bi-dot"></i> Masukkan jumlah kasbon yang diajukan tanpa menggunakan tanda titik
                         (contoh: 50000).</li>
                     <li><i class="bi bi-dot"></i> Nominal akan otomatis ditampilkan dalam satuan Rupiah.</li>
                 </ul>
             </div>
-
-
             <div class="col-12">
-                <h4 class="fw-semibold">6. <b>Klik Submit,</b> setelah semua data diisi dengan benar, klik tombol Submit
+                <h4 class="fw-normal mb-4">6. <b>Klik Submit,</b> setelah semua data diisi dengan benar, klik tombol Submit
                     untuk mengajukan Kasbon. Pengajuan Anda akan diproses dan menunggu persetujuan dari atasan atau HR. Atau
                     anda juga dapat mengklik tombol Draft untuk menyimpan pengajuan sebagai Draf, sehingga dapat diedit atau
                     dikirim di kemudian hari.
                 </h4>
-                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi4.png') }}" alt="Fitur KPI">
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/4.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">7. Pilih <b>SUBMIT</b> untuk melanjutkan pengajuan atau <b>CANCEL</b> untuk
+                <h4 class="fw-normal mb-4">7. Pilih <b>SUBMIT</b> untuk melanjutkan pengajuan atau <b>CANCEL</b> untuk
                     membatalkan.</h4>
-                <img class="img-custom mb-2" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi5.png') }}"
-                    alt="Fitur KPI">
-
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/5.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">8. Anda akan diarahkan otomatis ke halaman <b>Cash Advance</b>, Lalu klik <b></b>
+                <h4 class="fw-normal mb-4">8. Anda akan diarahkan otomatis ke halaman <b>Cash Advance</b>, Lalu klik <b></b>
                     untuk menampilkan informasi lengkap pengajuan.</h4>
-                <img class="img-custom mb-2" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi6.png') }}"
-                    alt="Fitur KPI">
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/6.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
-
             <div class="col-12">
-                <h4 class="fw-semibold">9.Anda akan diarahkan ke halaman Detail, yang menampilkan informasi lengkap
-                    pengajuan, yaitu:
-                    - Name: Jenis kasbon yang diajukan.
-                    - Status: Pengajuan akan berubah menjadi "Approved" apabila permohonan kasbon telah disetujui oleh
-                    atasan. Jika status masih "Pending", artinya permohonan masih menunggu proses persetujuan.
-                    - Amount: Jumlah nominal kasbon yang diajukan.
-                    - Date: Tanggal pengajuan kasbon.
+                <h4 class="fw-normal mb-4">9.Anda akan diarahkan ke halaman Detail, yang menampilkan informasi lengkap
+                    pengajuan, yaitu:<br><br>
+                    <ul class="d-flex flex-column gap-2 ms-4" style="list-style: disc;">
+                        <li>Name: Jenis kasbon yang diajukan.</li>
+                        <li>Status: Pengajuan akan berubah menjadi "Approved" apabila permohonan kasbon telah disetujui
+                            oleh
+                            atasan. Jika status masih "Pending", artinya permohonan masih menunggu proses persetujuan.</li>
+                        <li>Amount: Jumlah nominal kasbon yang diajukan.</li>
+                        <li>Date: Tanggal pengajuan kasbon.</li>
+                    </ul>
                 </h4>
-                <img class="img-custom mb-2" src="{{ asset('assets/img/mobile/fiturCashAdvance/kpi7.png') }}"
-                    alt="Fitur KPI">
+                <h4 class="mb-4 fw-normal">Sebelum</h4>
+                <img class="img-custom mb-4" src="{{ asset('assets/img/mobile/fiturCashAdvance/7-1.png') }}"
+                    alt="Fitur Cash Advance">
+                <h4 class="mb-4 fw-normal">Sesudah</h4>
+                <img class="img-custom" src="{{ asset('assets/img/mobile/fiturCashAdvance/7-2.png') }}"
+                    alt="Fitur Cash Advance">
             </div>
         </div>
         {{-- /Page Content --}}
